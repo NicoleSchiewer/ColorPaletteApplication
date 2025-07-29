f2ebefc7ca1afc6c11f0a61264d632d4c793dfb2
Nikki Schiewer <Schiewer.Nicole@gmail.com>
2025-07-29 09:27:22 -0500

    Troubleshooting CORS issues.

--- Backend/ColorPaletteApp/Program.cs
+++ Backend/ColorPaletteApp/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://color-palette-application.vercel.app")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
